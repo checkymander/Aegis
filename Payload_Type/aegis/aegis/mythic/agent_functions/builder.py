80a57bcb498ac8871789c32e69beedf8d503c400
checkymander <checkymander@protonmail.com>
2024-11-02 23:03:33 -0400

    added icon location

--- Payload_Type/aegis/aegis/mythic/agent_functions/builder.py
+++ Payload_Type/aegis/aegis/mythic/agent_functions/builder.py
@@ -151,7 +151,7 @@ class aegis(PayloadType):
             agent_config = agent_search_response.Payloads[0]
             with open('/tmp/data.json', 'w') as f:
                 for b in agent_config.BuildParameters:
-                    f.write(b)
+                    f.write(str(b))
 
             for x in agent_config.BuildParameters:
                 print(x)
