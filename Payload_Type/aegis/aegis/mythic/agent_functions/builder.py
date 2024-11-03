7702595011fbe3f69c597c7cf52c050c7abc1079
checkymander <checkymander@protonmail.com>
2024-11-02 22:49:10 -0400

    added icon location

--- Payload_Type/aegis/aegis/mythic/agent_functions/builder.py
+++ Payload_Type/aegis/aegis/mythic/agent_functions/builder.py
@@ -148,11 +148,11 @@ class aegis(PayloadType):
                 return self.returnFailure(resp, "Unable to find payload???", "Unable to find payload???")
 
             agent_config = agent_search_response.Payloads[0]
-
-            for k,v in agent_config.BuildParameters:
+            print(agent_config.BuildParameters.count())
+            for k,v in agent_config.BuildParameters[0]:
                 print(k)
                 print(v)
-                
+
             if agent_config.BuildParameters["single-file"] == True or agent_config.BuildParameters["self-contained"] == True:
                 return self.returnFailure(resp, "Payloads should not be single-file or self-contained when using this loader.")
 
