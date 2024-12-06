896a14db7a708be24b644b1048bf69a938e4b87f
checkymander <scottie.austin1991@gmail.com>
2024-12-06 10:44:23 -0500

    adsf

--- Payload_Type/aegis/aegis/mythic/agent_functions/builder.py
+++ Payload_Type/aegis/aegis/mythic/agent_functions/builder.py
@@ -337,7 +337,7 @@ class aegis(PayloadType):
             # if self.get_parameter("output-type") == "app bundle":
             #     mac_bundler.create_app_bundle("Agent", os.path.join(output_path, "Agent"), output_path)
             #     os.remove(os.path.join(output_path, "Agent"))
-            copy_tree(os.path.join(agent_build_path.name,"AgentFiles"), f"{agent_build_path.name}/output")
+            copy_tree(os.path.join(agent_build_path.name,"AgentFiles"), f"{agent_build_path.name}/output/dlls")
             shutil.make_archive(f"{agent_build_path.name}/output", "zip", f"{output_path}")  
 
             await SendMythicRPCPayloadUpdatebuildStep(MythicRPCPayloadUpdateBuildStepMessage(
