7fd370779265b276fd507ac5c90041b83d2e4518
checkymander <scottie.austin1991@gmail.com>
2024-12-06 12:34:00 -0500

    Added initial aegis mods

---
+++ Payload_Type/aegis/aegis/agent_code/Aegis/Aegis.Mod.Delay/AegisDelay.cs
@@ -0,0 +1,32 @@
+﻿using Aegis.Models.Interfaces;
+
+namespace Aegis.Mod.Delay
+{
+    public class AegisDelay : IMod
+    {
+        public async Task<bool> Check()
+        {
+            DateTime start = DateTime.Now;
+            Random random = new Random();
+            int d = random.Next(60000, 600000);
+            //Sleep for a random amount of time between 1 and 10 minutes
+            Thread.Sleep(d);
+
+            DateTime end = DateTime.Now;
+
+            double differential = GetSecondsBetween(start, end);
+
+            //If our number is significantly less than d (giving a 10% buffer) then we're being analyzed in a sandbox and should exit
+            if (differential < (d * 0.9))
+            {
+                return false;
+            }
+            return true;
+        }
+        public double GetSecondsBetween(DateTime earlierTimestamp, DateTime laterTimestamp)
+        {
+            TimeSpan timeDifference = laterTimestamp - earlierTimestamp;
+            return timeDifference.TotalSeconds;
+        }
+    }
+}
---
+++ Payload_Type/aegis/aegis/agent_code/Aegis/Aegis.Mod.DomainLookup/DomainLookup.cs
@@ -0,0 +1,71 @@
+﻿using Aegis.Models.Interfaces;
+
+namespace Aegis.Mod.DomainLookup
+{
+    public class DomainLookup : IMod
+    {
+        private Random rng = new Random();
+        private HttpClient httpClient = new HttpClient();
+
+        private void Shuffle(List<string> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                string value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+        public async Task<bool> Check()
+        {
+            List<string> j = new() {
+                "google.com",
+                "facebook.com",
+                "wikipedia.com",
+                "reddit.com",
+                "x.com",
+                "youtube.com",
+                "instagram.com",
+                "yahoo.com",
+                "cnn.com",
+                "msnbc.com",
+                "fox.com",
+                "foxnews.com",
+                "chatgpt.com",
+                "microsoftonline.com",
+                "netflix.com",
+                "bing.com",
+                "linkedin.com",
+                "office.com",
+                "amazon.com",
+                "weather.com"
+            };
+
+            Shuffle(j);
+
+            return await GetRequests(j);
+        }
+        private async Task<bool> GetRequests(List<string> urls)
+        {
+            foreach (string url in urls)
+            {
+                try
+                {
+                    HttpResponseMessage response = await httpClient.GetAsync(url);
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                }
+                catch (HttpRequestException e)
+                {
+                    //Fail out if any of the domain lookups don't work. Is this good or bad?
+                    return false;
+                }
+
+                Thread.Sleep(10000);
+            }
+            return true;
+        }
+    }
+}
